33cf4232434005e4f215b0a526068c6048ac8abb
FrancoRita <127884938+FrancoRita@users.noreply.github.com>
2025-08-09 15:10:06 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,1 @@
-// coloca o ano automaticamente no rodapé
-document.addEventListener('DOMContentLoaded', function(){
-  const ano = new Date().getFullYear();
-  const el = document.getElementById('ano');
-  if(el) el.textContent = ano;
-});
+
